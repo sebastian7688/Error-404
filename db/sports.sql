87d1fcb41f09695b359515c1c3f28dc1f893db4b
Lucio <Luchito14567@gmail.com>
2023-04-19 08:51:41 -0300

    cambio la base de datos

--- db/sports.sql
+++ db/sports.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-04-2023 a las 18:36:44
+-- Tiempo de generación: 19-04-2023 a las 13:41:31
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -18,43 +18,20 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `always_sporting`
+-- Base de datos: `sports`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `noticias`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int(255) UNSIGNED NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `correo` varchar(50) NOT NULL,
-  `contra` varchar(100) NOT NULL
+CREATE TABLE `noticias` (
+  `gf` varchar(100) NOT NULL,
+  `as` varchar(100) NOT NULL,
+  `ds` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
