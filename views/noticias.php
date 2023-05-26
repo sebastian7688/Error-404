131f549e9f01f2373b559f3a25ef801dc26097a8
Evelynuwu3456 <evelyn2night3@gmail.com>
2023-05-26 01:26:38 -0300

    fix

--- views/noticias.php
+++ views/noticias.php
@@ -6,12 +6,7 @@
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="index.php" class="link-info" style="text-decoration: none;">Inicio</a>
             </li>
-<<<<<<< HEAD
             <li class="breadcrumb-item active" aria-current="page"><a style="text-decoration: none;">Noticias</a>
-=======
-            <li class="breadcrumb-item active" aria-current="page"><a class="link-dark"
-                    style="text-decoration: none;">Noticias</a>
->>>>>>> 47d5b32a02406c643e6f2216ee8a7b757cf3fa39
             </li>
         </ol>
     </nav>
@@ -22,22 +17,14 @@
 <?php
 if (isset($_SESSION['usuario'])) {
     if ($_SESSION['usuario']['rol'] > 1) {
-<<<<<<< HEAD
 ?>
-=======
-        ?>
->>>>>>> 47d5b32a02406c643e6f2216ee8a7b757cf3fa39
         <div align="center">
             <button class="btn btn-primary me-md-2" type="button"><a href="add_notices.php" a>Agrega tus noticas</button>
             <button class="btn btn-primary" type="button"><a href="noticias.php" a>Mira tus noticias</button>
         </div>
         <br>
         <br>
-<<<<<<< HEAD
 <?php }
-=======
-    <?php }
->>>>>>> 47d5b32a02406c643e6f2216ee8a7b757cf3fa39
 } ?>
 <!-- Carousel Start -->
 
@@ -53,12 +40,10 @@ if (isset($_SESSION['usuario'])) {
                 <img class="position-absolute w-100 h-100" src="img/home/caru-1.jpg" style="object-fit: cover;">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 900px;">
-                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown"
-                            style="letter-spacing: 3px;">Spa & Beauty Center</h6>
+                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Spa & Beauty Center</h6>
                         <h3 class="display-3 text-capitalize text-white mb-3">Noticias del dia</h3>
                         <p class="mx-md-5 px-5">La mejor forma de empezar tu dia es con un buen cafecito :3</p>
-                        <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp"
-                            href="#">Noticias positivas</a>
+                        <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Noticias positivas</a>
                     </div>
                 </div>
             </div>
@@ -66,12 +51,10 @@ if (isset($_SESSION['usuario'])) {
                 <img class="position-absolute w-100 h-100" src="img/home/caru-2.jpg" style="object-fit: cover;">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 900px;">
-                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown"
-                            style="letter-spacing: 3px;">Spa & Beauty Center</h6>
+                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Spa & Beauty Center</h6>
                         <h3 class="display-3 text-capitalize text-white mb-3">Noticias noticiosas</h3>
                         <p class="mx-md-5 px-5">¿Ques mejor el helado de chocolate o el de chocolate marroc?</p>
-                        <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp"
-                            href="#">clikea para averiguarlo</a>
+                        <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp" href="#">clikea para averiguarlo</a>
                     </div>
                 </div>
             </div>
@@ -79,12 +62,10 @@ if (isset($_SESSION['usuario'])) {
                 <img class="position-absolute w-100 h-100" src="img/home/caru-3.png" style="object-fit: cover;">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 900px;">
-                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown"
-                            style="letter-spacing: 3px;">Spa & Beauty Center</h6>
+                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Spa & Beauty Center</h6>
                         <h3 class="display-3 text-capitalize text-white mb-3">Noticas debatientes</h3>
                         <p class="mx-md-5 px-5">¿Quien es mejor Maradora o Messi?</p>
-                        <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp"
-                            href="#">clikea para averiguarlo</a>
+                        <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp" href="#">clikea para averiguarlo</a>
                     </div>
                 </div>
             </div>
@@ -96,7 +77,6 @@ if (isset($_SESSION['usuario'])) {
 <br>
 
 <!---start cards1-->
-<<<<<<< HEAD
 <div class="row g-5">
     <div class="col-md-9">
         <div class="container-fluid pt-5 pb-3">
@@ -118,30 +98,10 @@ if (isset($_SESSION['usuario'])) {
                             <br>
                             <a href="#" class="btn btn-info" style="color: white">Leer mas ></a>
                         </div>
-=======
-<div class="container-fluid pt-5 pb-3">
-    <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4">
-        <span class="bg-secondary pr-3">Noticias destacadas</span>
-    </h2>
-    <div class="row px-xl-5">
-        <div class="col-lg-8 col-md-6 col-sm-6 pb-36">
-            <div class="product-item bg-light mb-4">
-                <div class="product-img position-relative overflow-hidden">
-                    <img class="img-fluid w-100" src="img/home/noti-6.jpg" alt="">
-                </div>
-                <div class="text-center py-4">
-                    <a class="h6 text-decoration-none text-truncate" href="">
-                        <h5>Nombre de la noticia</h5>
-                    </a>
-                    <div class="d-flex align-items-center justify-content-center mt-2">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
->>>>>>> 47d5b32a02406c643e6f2216ee8a7b757cf3fa39
                     </div>
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
         <br>
         <br>
         <!-----End card1---->
@@ -302,7 +262,6 @@ if (isset($_SESSION['usuario'])) {
             </div>
 
             <!---End cards3-->
-                <br>
                 <br>
             <!--Start carousel--->
             <h2 style="margin-left: 50px;">Voley</h2>
@@ -499,174 +458,13 @@ if (isset($_SESSION['usuario'])) {
                             <br>
                             <a href="#" class="btn btn-info" style="color: white">Leer mas ></a>
                         </div>
-=======
-        <div class="col-lg-4 col-md-4 col-sm-9 pb-8">
-            <div class="product-item bg-light mb-4">
-                <div class="product-img position-relative overflow-hidden">
-                    <img class="img-fluid w-100" src="img/home/noti-8.jpg" alt="">
-                </div>
-                <div class="text-center py-4">
-                    <a class="h6 text-decoration-none text-truncate" href="">
-                        <h5>Nombre de la noticia</h5>
-                    </a>
-                    <div class="d-flex align-items-center justify-content-center mt-2">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's
-                            content.<br>yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy<br>vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv<br>fffffffffffffffffffffffffffffffffff<br>ccccccccccccccccccccccccccccccc<br>cccccccccccccccccccccccccccccccccc<br>cccccccccccccccccccccccccccccccccccccccccccv<br>fffffffffffffffffffffffffffff<br>dddddddddddddddefreertyhtghtddddddddddddddddd<br>
-                        </p>
->>>>>>> 47d5b32a02406c643e6f2216ee8a7b757cf3fa39
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-<<<<<<< HEAD
  --slider End--->
-=======
-<!---Start cards2-->
-<div class="container-fluid pt-5 pb-3">
-    <div class="container">
-
-        <div class="card" style="width: 28rem;">
-            <img src="" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-
-        <br>
-        <div align="center">
-            <div class="card" style="width: 28rem;">
-                <img src="" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <br>
-        <div align="right">
-            <div class="card" style="width: 35rem;">
-                <img src="" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</div>
-<!---End cards2-->
-<br>
-<br>
-<br>
-<!---Start cards3-->
-<div class="container-fluid pt-5 pb-3">
-    <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Noticias
-            :3</span></h2>
-    <div class="row px-xl-5">
-        <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-            <div class="product-item bg-light mb-4">
-                <div class="product-img position-relative overflow-hidden">
-                    <img class="img-fluid w-100" src="img/home/noti-6.jpg" alt="">
-                    <div class="product-action">
-                        <a class="btn btn-outline-dark btn-square" href="" style="background-color:rgb(3 158 207);"
-                            style="color:rgb(3 158 207);"><i class="fa fa-search"></i></a>
-                    </div>
-                </div>
-                <div class="text-center py-4">
-                    <a class="h6 text-decoration-none text-truncate" href="">
-                        <h5>Nombre de producto</h5>
-                    </a>
-                    <div class="d-flex align-items-center justify-content-center mt-2">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                    <br>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-            <div class="product-item bg-light mb-4">
-                <div class="product-img position-relative overflow-hidden">
-                    <img class="img-fluid w-100" src="img/home/noti-8.jpg" alt="">
-                    <div class="product-action">
-                        <a class="btn btn-outline-dark btn-square" href="" style="background-color:rgb(3 158 207);"
-                            style="color:rgb(3 158 207);"><i class="fa fa-search"></i></a>
-                    </div>
-                </div>
-                <div class="text-center py-4">
-                    <a class="h6 text-decoration-none text-truncate" href="">
-                        <h5>Nombre de producto</h5>
-                    </a>
-                    <div class="d-flex align-items-center justify-content-center mt-2">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                    <br>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-            <div class="product-item bg-light mb-4">
-                <div class="product-img position-relative overflow-hidden">
-                    <img class="img-fluid w-100" src="img/home/noti-8.jpg" alt="">
-                    <div class="product-action">
-                        <a class="btn btn-outline-dark btn-square" href="" style="background-color:rgb(3 158 207);"
-                            style="color:rgb(3 158 207);"><i class="fa fa-search"></i></a>
-                    </div>
-                </div>
-                <div class="text-center py-4">
-                    <a class="h6 text-decoration-none text-truncate" href="">
-                        <h5>Nombre de producto</h5>
-                    </a>
-                    <div class="d-flex align-items-center justify-content-center mt-2">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                    <br>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-            <div class="product-item bg-light mb-4">
-                <div class="product-img position-relative overflow-hidden">
-                    <img class="img-fluid w-100" src="img/home/noti-8.jpg" alt="">
-                    <div class="product-action">
-                        <a class="btn btn-outline-dark btn-square" href="" style="background-color:rgb(3 158 207);"
-                            style="color:rgb(3 158 207);"><i class="fa fa-search"></i></a>
-                    </div>
-                </div>
-                <div class="text-center py-4">
-                    <a class="h6 text-decoration-none text-truncate" href="">
-                        <h5>Nombre de producto</h5>
-                    </a>
-                    <div class="d-flex align-items-center justify-content-center mt-2">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                    <br>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!---End cards3-->
->>>>>>> 47d5b32a02406c643e6f2216ee8a7b757cf3fa39
 
 
 
