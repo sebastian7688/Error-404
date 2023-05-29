a35020d2f454765b7b8688662602e7f8ffd4bb25
joaquinreyes2905 <mikemoble2905@gmail.com>
2023-05-28 21:30:08 -0300

    fix bug

--- seeker.php
+++ seeker.php
@@ -83,13 +83,13 @@ if ($busqueda <> '  ') {
                         } ?>
 
                         <div class="product-action">
-                            <a class="btn btn-outline-dark btn-square" href="" style="background-color:rgb(3 158 207);"
+                            <a class="btn btn-outline-dark btn-square" href="products_read_more.php?id=<?php echo $nuevos['id']?>" style="background-color:rgb(3 158 207);"
                                 style="color:rgb(3 158 207);"><i class="fa fa-search"></i></a>
                         </div>
                     </div>
 
                     <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">
+                        <a class="h6 text-decoration-none text-truncate" href="products_read_more.php?id=<?php echo $nuevos['id']?>">
                             <?php echo $nuevos['nombre_prod'] ?>
 
                             <div align="center" &nbsp;&nbsp;>
