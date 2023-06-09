08511d61569ec5f5e9ad959d57f460e0536d16e4
lean <matigom13@gmail.com>
2023-06-08 21:17:28 -0300

    Re-diseñe el formulario de agregar noticias

--- views/add_notice.php
+++ views/add_notice.php
@@ -12,9 +12,9 @@
         </ol>
     </nav>
 </div>
-<div class="container">   
+<div class="container">  
     <form class="add" action="" method="post" enctype="multipart/form-data">
-        <h1 class="titulo_1">Agregar noticia</h1>
+        <h1 class="titulo_1"><img src="img/icon.ico" alt="" >Agregar noticia</h1>
         <div class="input-container">
             <label for="inputEmail4" class="titulo_2">Añadir titulo de la noticia</label>
             <input type="text" class="input" name="title">
@@ -27,12 +27,15 @@
             <br>
             <label for="inputEmail4" class="titulo_2">Añadir la foto de portada</label>
             <!-- <label class="input-group-text" for="inputGroupFile01">Upload</label> -->
-            <input type="file" class="form-control border-dark" id="inputGroupFile01" name="principal_img" id="principal_img">
+            <div id="div_file">
+                <p id="texto">Add file</p>
+                <input type="file" class="form-control border-dark" id="btn_file" name="principal_img" id="principal_img">
+            </div>
         </div>
         <br>
-        <button type="submit" class="btn btn-success">
+        <button type="submit" class="btn btn-success" style="background:#00bfff; border-color:#00bfff; border-radius:3px; box-shadow: 1px 1px 3px #000;">
             Enviar
         </button>
-        <a href="index.php" class="btn btn-danger">Cancelar</a>
+        <a href="index.php" class="btn btn-danger" style="Background:#343a40; border-color:#343a40; border-radius:3px; box-shadow: 1px 1px 3px #000;">Cancelar</a>
     </form>
 </div></div></div></div></div></div></div></div></div>
