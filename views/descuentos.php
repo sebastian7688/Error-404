4a6101e7f32ebfe7da210f6c70ed806ed6c94aea
Lucio <luchito14567@gmail.com>
2023-06-23 17:41:52 -0300

    Inicio seccion añadir descuentos

---
+++ views/descuentos.php
@@ -0,0 +1,19 @@
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <form>
+  <div class="form-group" style="text-align:center;"><br>
+    <label for="formGroupExampleInput">Ingrese Descuento</label><br><br>
+    <input type="text" class="form-control" style="text-align: center;" id="formGroupExampleInput" placeholder="Example input"><br><br>
+ <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button><br><br>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">1 mes</a></li>
+    <li><a class="dropdown-item" href="#">2 meses</a></li>
+    <li><a class="dropdown-item" href="#">3 meses</a></li>
+  </ul>
+</div>
+<button type="button" style="text-align: right;"  class="btn btn-success">Confirmar</button>
+</form>
