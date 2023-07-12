6dee59478b983812bb89050243fafe2c29fe66be
lean <matigom13@gmail.com>
2023-07-12 17:43:23 -0300

    Re-diseñe la seccion leer más de productos

--- views/products_read_more.php
+++ views/products_read_more.php
@@ -1,4 +1,5 @@
 <link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="css/read_more.css">
 <script src="//assets.jumpseller.com/public/jquery-3.3.1.min.js"></script>
 <div class="container">
     <nav aria-label="breadcrumb">
@@ -7,52 +8,41 @@
             </li>
             <li class="breadcrumb-item"><a href="shop.php" class="link-info" style="text-decoration: none;">Tienda</a>
             </li>
-            <li class="breadcrumb-item active" aria-current="page"><a style="text-decoration: none;">Producto</a>
+            <li class="breadcrumb-item active" aria-current="page"><a style="text-decoration: none;">Comentar</a>
             </li>
         </ol>
     </nav>
 </div>
 <?php foreach ($productos as $producto) {
     ?>
-    <div class="container border-top">
-
-
-
-
-        <!-- Page Heading -->
-        <div class="row">
-            <div class="col-12">
-                <br><br>
-                <h3 class="page-header">
-                    <?php echo $producto['nombre_prod'] ?>
-                </h3>
-            </div>
-        </div>
+    <div class="container" style="box-shadow: 0px 0px 15px #aaa;">
+        
         <!-- /.row -->
         <div class="row">
             <div class="col-lg-6 mb-4" >
                 <div class="" >
                         <div id="product-carousel" class="carousel slide">
-                    <div class="main-product-image space" onMouseover="this.style.box-shadow='1px 1px 1px black'" onMouseout="this.style.box-shadow='1px 1px 1px black'">
-                            <div class="carousel-inner" role="listbox" >
-                                <div class="carousel-item active">
-                                    <br>
-                                    <?php
-                                    if (file_exists('img/productos/' . $producto['id'] . '/principal.jpg')) { ?>
-                                        <img id="first-image" alt="" class="img-fluid" style="width:90%; height:60%;" 
-                                            src="img/productos/<?php echo $producto['id']; ?>/principal.jpg">
+                            <div class="main-product-image space" >
+                                <div class="carousel-inner" role="listbox" >
+                                    <div class="carousel-item active imagen">
+                                        <br>
                                         <?php
-                                    } ?>
+                                        if (file_exists('img/productos/' . $producto['id'] . '/principal.jpg')) { ?>
+                                            <img id="first-image" alt="" class="img-fluid" style="width:90%; height:60%;" 
+                                            src="img/productos/<?php echo $producto['id']; ?>/principal.jpg" >
+                                        <?php } ?>
                                 </div>
                             </div>
                         </div>
-                        <a class="carousel-control-prev" href="#product-carousel" role="button" data-slide="prev" style="color:;">
+                        <a class="carousel-control-prev" href="#product-carousel" role="button" data-slide="prev" style="color: #aaa;">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
+                            <i class="fa-solid fa-chevron-left"></i>
                         </a>
-                        <a class="carousel-control-next" href="#product-carousel" role="button" data-slide="next">
+                        <a class="carousel-control-next" href="#product-carousel" role="button" data-slide="next" style="color: #aaa;">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only">Next</span>
+                            <i class="fa-solid fa-chevron-right"></i>
                         </a>
                     </div>
                 </div>
@@ -66,14 +56,22 @@
                     <a class="thumbs" data-image="3" href="#"><img src="https://cdnx.jumpseller.com/bootstrap/image/14896546/thumb/120/150?1614272621" alt="Wacom Bamboo Tablet" /></a>
 
                 </div>-->
-
+                                                                      
             </div>
 
-            <div class="col-lg-6" style="border:1px solid #aaa; border-radius:5px; padding:3%;">
+            <div class="col-lg-6" style="padding:3%;">
                 <form class="form-horizontal" action="/cart/add/224300" method="post" enctype="multipart/form-data"
                     name="buy" style="border-bottom:1px solid #aaa; margin-bottom:15px;">
 
                     <!-- Product Price  -->
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="page-header">
+                                <?php echo $producto['nombre_prod'] ?>
+                            </h3>
+                        </div>
+                     </div>
+
                     <div class="form-group price_elem row">
                         <div class="col-sm-8 col-md-9">
                             <span class="product-form-price" id="product-form-price" style="font-size:250%; color: #000;;">$
@@ -100,33 +98,92 @@
                         <div class="form-group row">
                             <div class="col-sm-8 col-md-9">
                                 <input type="number" class="qty form-control" name="cant" id="input-qty" name="qty"
-                                    max="<?php echo $producto['cantidad'] ?>" min="1" placeholder="Cantidades">
+                                    max="<?php echo $producto['cantidad'] ?>" min="1" placeholder="Cantidades" style="border-radius:5px;">
                             </div>
                         </div>
                         <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>" />
                         <div style="display:flex;">
-                            <input type="submit" class="btn" style="width:100%; background: #00BFFF; border: 1px solid #00BFFF; color:#3D464D;" value="Compra Ahora" onMouseover="this.style.background='rgb(0 172 229)'" onMouseout="this.style.background='#00BFFF'">
-                            <input type="submit" Class="btn" style="width:100%; background: #e1e1e1; border: 1px solid #e1e1e1; color:#3D464D;" value="Agregar al carito" onMouseover="this.style.background='#cdcccc'" onMouseout="this.style.background='#e1e1e1'">
+                            <input type="submit" class="btn" style="width:100%; background: #00BFFF; border: 1px solid #00BFFF; border-radius:5px 0px 0px 5px; color:#3D464D; color: #fff;" value="Compra Ahora" onMouseover="this.style.background='rgb(0 172 229)'" onMouseout="this.style.background='#00BFFF'">
+                            <input type="submit" Class="btn" style="width:100%; background: #e1e1e1; border: 1px solid #e1e1e1; border-radius:0px 5px 5px 0px; color:#3D464D; color: #000;" value="Agregar al carito" onMouseover="this.style.background='#cdcccc'" onMouseout="this.style.background='#e1e1e1'">
                         </div>
 
                     </form>
                         <form class="form-horizontal" action="/cart/add/224300" method="post" enctype="multipart/form-data"
                     name="buy" style="border-top:1px solid #aaa; margin-top:15px; padding-top:3%;">
-                    <a class="btn btn-info text-light" href="editar_producto.php"><i class="fa-solid fa-pen-to-square"></i></a>
-                    <a class="btn btn-success text-light"><i class="fa-solid fa-percent"></i></a>
-                    <a class="btn btn-danger text-light"><i class="fa-solid fa-trash"></i></a>
-
-
-                        <div class="form-group row " style="margin-top:3%;">
-                            <!-- <label class="col-sm-3 col-md-3 form-control-label"></label> -->
-                            <div class="col-sm-9 col-md-9">
-                                <p>
-                                    <?php echo $producto['descripcion'] ?>
-                                </p>
-                            </div>
-                        </div>
+                    <a class="btn btn-info text-light" href="editar_producto.php" style="border-radius:5px;" title="Editar"><i class="fa-solid fa-pen-to-square"></i></a>
+                    <a class="btn btn-success text-light" style="border-radius:5px;" title="Descuento"><i class="fa-solid fa-percent"></i></a>
+                    <a class="btn btn-danger text-light" style="border-radius:5px;" title="Borrar"><i class="fa-solid fa-trash"></i></a>
                     </form>
                 </form>
+            </div>
+        </div>
+        <div class="container" style="border-top:1px solid #aaa; border-bottom:1px solid #aaa; padding-top:2%;">
+            <div class="form-group row ">
+                <div class="col-sm-8 col-md-9 description">
+                    <h3 class="page-header">Descripción</h3>                
+                    <p>
+                        <?php echo $producto['texto'] ?>
+                    </p>
+                </div>
+            </div>                            
+        </div>
+        <div class="container">
+            <div class="row comentarios justify-content-center">
+                <div class="col-12">
+                    <form action="" class="form_comentarios d-flex justify-content-end flex-wrap">
+                        <textarea name="" id="" placeholder="Comentario"></textarea>                
+                        <button class="btn" type="button" onMouseover="this.style.color='white'">Comentar</button>
+                    </form>
+                    <div class="media">
+                        <img src="img/group/mike.jpg" width="64" height="64" alt="">
+                        <div class="media-body">
+                            <p class="nombre">Nombre</p>
+                            <p class="comentario">Comentario</p>
+                            <div class="botones text-right">
+                                <a href="">Responder</a>
+                                <a href="">Editar</a>
+                                <a href="">Borrar</a>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="media">
+                        <img src="img/group/seba.jpg" width="64" height="64" alt="">
+                        <div class="media-body">
+                            <p class="nombre">Nombre</p>
+                            <p class="comentario">Comentario</p>
+                            <div class="botones text-right">
+                                <a href="">Responder</a>
+                                <a href="">Editar</a>
+                                <a href="">Borrar</a>
+                            </div>
+                            <div class="media">
+                        <img src="img/group/eve.jpg" width="64" height="64" alt="">
+                        <div class="media-body">
+                            <p class="nombre">Nombre</p>
+                            <p class="comentario">Comentario</p>
+                            <div class="botones text-right">
+                                <a href="">Responder</a>
+                                <a href="">Editar</a>
+                                <a href="">Borrar</a>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                    <div class="media">
+                        <img src="img/group/eve.jpg" width="64" height="64" alt="">
+                        <div class="media-body">
+                            <p class="nombre">Nombre</p>
+                            <p class="comentario">Comentario</p>
+                            <div class="botones text-right">
+                                <a href="">Responder</a>
+                                <a href="">Editar</a>
+                                <a href="">Borrar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>                                
             </div>
         </div>
     </div>
@@ -144,8 +201,6 @@
             });
         });
     </script>
-
-
-
-
 <?php } ?>
+
+
