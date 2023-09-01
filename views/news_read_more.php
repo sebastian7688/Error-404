49d23587e108800d9ec39b5886457265a61401b2
lean <matigom13@gmail.com>
2023-08-31 22:49:17 -0300

    Agregue detalles a las seccion leer mas de noticias

--- views/news_read_more.php
+++ views/news_read_more.php
@@ -14,16 +14,16 @@
     </nav>
 </div>
 
-<div class="container">
+<div class="container" style="box-shadow:0px 0px 15px #aaa;">
     <div class="col-12">
-        <h1 style="text-align:center; margin-bottom:25px; font-size:350%; color:rgb(21, 30, 36);">Sorteo de la libertadores</h1>
+        <h1 style="text-align:center; margin-bottom:25px; padding-top:25px; font-size:350%; color:rgb(21, 30, 36);">Sorteo de la libertadores</h1>
         <div class="containerImagen">
             <img class="imgFluid"  src="img/noticias/libertadores.png">
         </div>
-        <h2 style="margin-top:15px;">Así se jugarán los octavos de la Copa Libertadores</h2>
+        <h2 style="margin-top:15px; border-left:5px solid #00BFFF; padding-left:5px; color:#1e1e1e;">Así se jugarán los octavos de la Copa Libertadores</h2>
     </div>
     
-    <div class="col-12">
+    <div class="col-12" style="">
         <div class="media" style="padding-top:15px; padding-bottom:15px; border-bottom:1px solid #bfbfbf;">
             <img src="img/group/eve.jpg" width="64" height="64" alt="">
             <div class="media-body">
@@ -33,44 +33,33 @@
         </div>
     </div>                              
 
-    <div class="col-12">
-        <div class="container" style="padding-top:1%;">
+     <div class="col-12">
+        <div class="container" style="padding-top:1%; padding-bottom:1%;">
             <div class="form-group row">
                 <div class="col-sm-8 col-md-12">              
-                    <p style="font-size:25px; letter-spacing:0;">
+                    <p class="text" style="font-size:25px; letter-spacing:0; ">
                      Para definir cada cruce de octavos, se sacaba una bola del bombo 2 y luego una del bolillero 1. Este procedimiento se llevó a cabo hasta completar ocho enfrentamientos, a los que se les asignó una letra de la A a la H según el orden de aparición (es decir, el primer cruce en el sorteo recibió la letra A y el último la H).
-
 
                     Estos son los cruces de octavos de la Libertadores tras el sorteo de este miércoles:
 
-
                     A. Atlético Nacional (Colombia) vs. Racing (Argentina)
-
 
                     B. Bolívar (Bolivia) vs. Athletico Paranaense (Brasil)
 
-
                     C. Flamengo (Brasil) vs. Olimpia (Paraguay)
-
 
                     D. Atlético Mineiro (Brasil) vs. Palmeiras (Brasil)
 
-
                     E. Deportivo Pereira (Colombia) vs. Independiente del Valle (Ecuador)
-
 
                     F. Argentinos Juniors (Argentina) vs. Fluminense (Brasil)
 
-
                     G. River Plate (Argentina) vs. Inter de Porto Alegre (Brasil)
-
 
                     H. Nacional (Uruguay) vs. Boca Juniors (Argentina)
 
-
                     Los enfrentamientos más atractivos y los posibles cruces futuros
                     Si bien en los octavos de final no tendremos ningún clásico, hay algunos encuentros que llaman la atención debido a que se tratan de clubes del mismo país o de naciones con rivalidad directa.
-
 
                     ablamos de los partidos de las llaves D, F y G: en la primera tendremos a Atlético Mineiro, campeón de Libertadores en 2013, y Palmeiras, campeón de la liga brasileña en 2022 y club que busca su cuarta Libertadores en su historia; la llave F está protagonizada por Argentinos Juniors, cuyo primero y último campeonato de Libertadores se remonta a 1985, y Fluminense, club que busca por primera vez el título de este torneo; y, por último, el duelo entre River Plate, equipo que va por su quinta Libertadores, e Inter de Porto Alegre, que intentará conseguir su tercera Copa Libertadores y primera desde 2010.
                     </p>
