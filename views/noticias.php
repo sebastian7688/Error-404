0e60d5f45e7056f75203e9d4a5ceb6d9702adc4c
sebastian7688 <sebastianpardo583@gmail.com>
2023-11-03 08:04:38 -0300

    Fix

--- views/noticias.php
+++ views/noticias.php
@@ -30,7 +30,7 @@
                 <img class="position-absolute w-100 h-100" src="img/home/caru-1.jpg" style="object-fit: cover;">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 700px;">
-                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Spa & Beauty Center</h6>
+                    <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Arba.Net Inc</h6>
                         <h3 class="display-3 text-capitalize text-white mb-3">Noticias del dia</h3>
                         <p class="mx-md-5 px-5">La mejor forma de empezar tu dia es con un buen cafecito :3</p>
                         <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Noticias positivas</a>
@@ -41,22 +41,22 @@
                 <img class="position-absolute w-100 h-100" src="img/home/caru-2.jpg" style="object-fit: cover;">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 900px;">
-                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Spa & Beauty Center</h6>
+                    <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Arba.Net Inc</h6>
                         <h3 class="display-3 text-capitalize text-white mb-3">Noticias noticiosas</h3>
                         <p class="mx-md-5 px-5">¿Ques mejor el helado de chocolate o el de chocolate marroc?</p>
                         <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp" href="#">clikea para averiguarlo</a>
-                    </div>
+                   </div>
                 </div>
             </div>
             <div class="carousel-item position-relative" style="min-height: 80vh;">
                 <img class="position-absolute w-100 h-100" src="img/home/caru-3.png" style="object-fit: cover;">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 900px;">
-                        <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Spa & Beauty Center</h6>
+                    <h6 class="text-white text-uppercase mb-3 animate__animated animate__fadeInDown" style="letter-spacing: 3px;">Arba.Net Inc</h6>
                         <h3 class="display-3 text-capitalize text-white mb-3">Noticas debatientes</h3>
                         <p class="mx-md-5 px-5">¿Quien es mejor Maradora o Messi?</p>
                         <a class="btn btn-outline-light py-3 px-4 mt-3 animate__animated animate__fadeInUp" href="#">clikea para averiguarlo</a>
-                    </div>
+                   </div>
                 </div>
             </div>
         </div>
